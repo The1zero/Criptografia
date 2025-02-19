773a1d0ce69cb2650054f5d5733e751aef79b18c
Salgado Valdés Andrés <120762809+The1zero@users.noreply.github.com>
2025-02-18 19:32:22 -0600

    Create Forma de probar los programas en .py

---
+++ Forma de probar los programas en .py
@@ -0,0 +1,4 @@
+-Para correr el programa utilizamos el siguiente compilador online: https://www.programiz.com/python-programming/online-compiler/
+
+-En caso de que use visual studio code debera ir a la carpeta donde tenga el codigo guardado. Una vez dentro de esa carpeta y que en la terminal de visual studio nos aparezca que ya estamos ahi,
+ejecutamos el comando "python nombredelarchivo.py"  
